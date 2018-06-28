86c3260ba7f82e6f9b0d9a12fb1dcb3a3f2fa3d4
Vitaly Kiselev <st411ar@ya.ru>
2018-06-28 16:24:36 +0300

    created intro component, other components hided from the sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,23 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 
 import { ExampleModel } from './models/example.model';
 import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component';
+import { IntroComponent } from './intro/intro.component';
 
 export const examples: ExampleModel[] = [
   {label:	'Intro'},
-  {label:	'NgFor'},
-  {label:	'NgSwitch'},
-  {label:	'NgStyle'},
-  {label:	'NgClass'},
-  {label:	'NgNonBindable'}
+//  {label:	'NgFor'},
+//  {label:	'NgSwitch'},
+//  {label:	'NgStyle'},
+//  {label:	'NgClass'},
+//  {label:	'NgNonBindable'}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    SidebarItemComponent
+    SidebarItemComponent,
+    IntroComponent
   ],
   imports: [
     BrowserModule
