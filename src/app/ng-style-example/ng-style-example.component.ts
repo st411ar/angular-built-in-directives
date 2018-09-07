251125681fc12dc445aa9955ecab77b9c94056b2
Vitaly Kiselev <st411ar@ya.ru>
2018-09-07 22:14:44 +0300

    removed redundant features

--- src/app/ng-style-example/ng-style-example.component.ts
+++ src/app/ng-style-example/ng-style-example.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ng-style-example',
-  templateUrl: './ng-style-example.component.html',
-  styleUrls: ['./ng-style-example.component.css']
+	selector: 'app-ng-style-example',
+	templateUrl: './ng-style-example.component.html'
 })
 export class NgStyleExampleComponent implements OnInit {
 
-  constructor() { }
+	constructor() {}
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
 
 }
