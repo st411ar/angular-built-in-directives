031c41c8aed0e5047cb2c2b878ee7064ce7c4a4b
Vitaly Kiselev <st411ar@ya.ru>
2018-09-07 22:53:51 +0300

    completed ng-style-example component

--- src/app/ng-style-example/ng-style-example.component.ts
+++ src/app/ng-style-example/ng-style-example.component.ts
@@ -5,9 +5,31 @@ import { Component, OnInit } from '@angular/core';
 	templateUrl: './ng-style-example.component.html'
 })
 export class NgStyleExampleComponent implements OnInit {
+	color: string;
+	fontSize: number;
+	style: {
+		'background-color': string,
+		'border-radius': string,
+		border?: string,
+		width?: string,
+		height?: string
+	};
 
 	constructor() {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.fontSize = 16;
+		this.color = 'blue';
+		this.style = {
+			'background-color': '#ccc',
+			'border-radius': '50px',
+			'height': '30px',
+			'width': '30px'
+		};
+	}
 
+	apply(color: string, fontSize: number): void {
+		this.color = color;
+		this.fontSize = fontSize;
+	}
 }
