c7cf81d9a16784601389033b3482dfa31f08f3bb
Vitaly Kiselev <st411ar@ya.ru>
2018-09-10 11:23:48 +0300

    completed ng-for-example component

--- src/app/ng-for-example/ng-for-example.component.ts
+++ src/app/ng-for-example/ng-for-example.component.ts
@@ -5,8 +5,47 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './ng-for-example.component.html'
 })
 export class NgForExampleComponent implements OnInit {
+	cities: string[];
+	people: Object[];
+	peopleByCity: Object;
+
 	constructor() {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		this.cities = ['Miami', 'Sao Paulo', 'New York'];
+		this.people = [
+			{ name: 'Anderson', age: 35, city: 'Sao Paulo' },
+			{ name: 'John', age: 12, city: 'Miami' },
+			{ name: 'Peter', age: 22, city: 'New York' }
+		];
+		this.peopleByCity = [
+			{
+				city: 'Miami',
+				people: [
+					{
+						name: 'John',
+						age: 12
+					},
+					{
+						name: 'Angel',
+						age: 22
+					}
+				]
+			},
+			{
+				city: 'Sao Paulo',
+				people: [
+					{
+						name: 'Anderson',
+						age: 35
+					},
+					{
+						name: 'Felipe',
+						age: 36
+					}
+				]
+			}
+		];
+	}
 
 }
